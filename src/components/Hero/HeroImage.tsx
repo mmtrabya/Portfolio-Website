05d3dac0f98228e88b4ea018403b64cf4d0c073d
mmtrabya <midotarabay66@gmail.com>
2024-12-27 03:37:50 +0200

    Final Deployment

--- src/components/Hero/HeroImage.tsx
+++ src/components/Hero/HeroImage.tsx
@@ -4,7 +4,8 @@ export const HeroImage = () => {
   return (
     <div className="relative w-150 h-150 md:w-[650px] md:h-[650px]">
       <img
-        src="/67c26305-e25f-4265-b390-a8f550c72952.png"  // Referencing directly from public folder (no 'public' in the path)
+        src="https://mmtrabya.github.io/Portoflio-Website/67c26305-e25f-4265-b390-a8f550c72952.png
+"  // Referencing directly from public folder (no 'public' in the path)
         alt="Profile"
         className="w-full h-full object-contain"
       />
