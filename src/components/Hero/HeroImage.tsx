0ceb5ba5e959e68547648b14f8ace8615ea63466
mmtrabya <midotarabay66@gmail.com>
2024-12-27 03:27:36 +0200

    Final Deployment

--- src/components/Hero/HeroImage.tsx
+++ src/components/Hero/HeroImage.tsx
@@ -4,7 +4,7 @@ export const HeroImage = () => {
   return (
     <div className="relative w-150 h-150 md:w-[650px] md:h-[650px]">
       <img
-        src="67c26305-e25f-4265-b390-a8f550c72952.png" // path from public folder
+        src="/67c26305-e25f-4265-b390-a8f550c72952.png" // path from public folder
         alt="Profile"
         className="w-full h-full object-contain"
       />
